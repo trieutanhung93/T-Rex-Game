fa97f50bb4ac4416f5665a06d1c6d4a8326b10e5
HungBee <trieutanhung93@gmail.com>
2021-10-27 01:25:25 +0700

    set up phaser

---
+++ src/game.ts
@@ -0,0 +1,27 @@
+import 'phaser';
+
+var config = {
+  type: Phaser.WEBGL,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  scene: {
+      preload: preload,
+      create: create
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.image('bunny', 'assets/sprites/pip.png');
+}
+
+function create ()
+{
+  var bunny = this.add.image(400, 300, 'bunny');
+
+  bunny.alpha = 0.5;
+}
+
