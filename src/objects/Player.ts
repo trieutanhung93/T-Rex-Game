4d34db298ee0df5dfda570b0193774ee9056c11f
HungBee <trieutanhung93@gmail.com>
2021-10-26 23:43:06 +0700

    add sprites

--- src/objects/Player.ts
+++ src/objects/Player.ts
@@ -1,5 +1,5 @@
 import Phaser from "phaser";
 
 export class Player extends Phaser.GameObjects.Image{
-    
+
 }
---
+++ src/scenes/MainMenuScene.ts
@@ -0,0 +1,5 @@
+import Phaser from "phaser";
+
+export class MainMenuScene extends Phaser.Scene{
+    
+}
